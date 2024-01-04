28e354340d2d7f76f6a3ef266ba60b0cbf4c16c7
Sujal Neupane <121492848+SujalNeupane9@users.noreply.github.com>
2024-01-04 14:03:34 +0545

    Update ingest_data.py

--- src/ingest_data.py
+++ src/ingest_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from src.logger import get_console_logger
+from src.paths import DATA_DIR
 from typing import Optional
+
 
 logger = get_console_logger('Data-ingestion')
 
 ## The data import from API is also imported here if there is any..
 def ingest_data()-> pd.DataFrame:
-    data = pd.read_csv("E:\dl\Breast-Cancer-Survival-Prediction\data\data.csv")
+    data = pd.read_csv(DATA_DIR)
     return data
